ef80d2d5aeb89dc55d0bdf4d0af89e27ffbb92a4
NoRicePls <xuande.soong@gmail.com>
2021-01-19 21:54:25 +0800

    I have updated text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello People.
+          Hello human, I come in peace.
         </p>
         <a
           className="App-link"
